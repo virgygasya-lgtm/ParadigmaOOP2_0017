89d2af43a3a67471194f7df7268259dcced133d4
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-18 10:53:27 +0700

    penambahanclassbudidenganpewarisangandasertafungsi.cpp

--- penambahanclassbudidenganpewarisangandasertafungsi.cpp
+++ penambahanclassbudidenganpewarisangandasertafungsi.cpp
@@ -28,3 +28,18 @@ public:
         cout << "pelajar dibuat\n" << endl;
     }
 };
+class budi : public pekerja, public pelajar {
+public:
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main() {
+    budi a(12);
+    return 0;
+}
