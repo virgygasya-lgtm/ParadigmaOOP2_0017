84d52a897173180f6ee264f3532179cfa3c345d6
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-18 10:29:25 +0700

    penambahanobyekdanpesandariseseorang.cpp

--- penambahanobyekdanpesandariseseorang.cpp
+++ penambahanobyekdanpesandariseseorang.cpp
@@ -35,3 +35,24 @@ int main() {
     //a.seseorang::pesan();
     //b.seseorang::pesan();
 }
+class lia : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+
+    //a.seseorang::pesan();
+    //b.seseorang::pesan();
+    return 0;
+}
