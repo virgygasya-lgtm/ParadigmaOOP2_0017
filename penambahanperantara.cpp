a60ffce2c78ef35cbdfb450c5133a6f50373ec9c
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-18 10:36:13 +0700

    penambahanperantara.cpp

---
+++ penambahanperantara.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
