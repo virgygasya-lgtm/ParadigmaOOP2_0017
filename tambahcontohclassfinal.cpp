d4b4c0efc24565b009684b8641514b7f3afb873f
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-18 10:58:47 +0700

    tambahcontohclassfinal.cpp

---
+++ tambahcontohclassfinal.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
