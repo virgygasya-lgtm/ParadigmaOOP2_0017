15201f04f727391d560123bbf6fa45e54580ec59
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-18 11:00:54 +0700

    penambahanderivedclassdanperkenalan.cpp

--- penambahanderivedclassdanperkenalan.cpp
+++ penambahanderivedclassdanperkenalan.cpp
@@ -14,3 +14,16 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
