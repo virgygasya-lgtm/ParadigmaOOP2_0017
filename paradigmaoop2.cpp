f3f5fcde5b4a15a48d6bd2379dd1786d6c028e5d
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-05-18 10:19:03 +0700

    paradigmaoop2.cpp

---
+++ paradigmaoop2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan(){
+    //     cout<<"Pesan dari seseorang"<<endl;
+    // }
+};
+
+class joko : public seseorang {
+public:
+    void pesan() {
+        cout << "Pesan dari joko" << endl;
+    }
+};
